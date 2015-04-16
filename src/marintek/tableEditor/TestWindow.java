b6cea9eed6049c5c59b978418fd23f31396d041e
Matthias P. Nowak <matthias.nowak@marintek.sintef.no>
2015-04-16 17:11:06 +0200

    trying with added column and having cursor visible at all times

--- src/marintek/tableEditor/TestWindow.java
+++ src/marintek/tableEditor/TestWindow.java
@@ -1,6 +1,5 @@
 package marintek.tableEditor;
 
-
 import java.util.ArrayList;
 
 import marintek.tableEditor.TestData.Entry;
@@ -35,16 +34,15 @@ import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 
 public class TestWindow extends Shell {
 
 	private static final String COL_B = "ColB";
 	private static final String COL_A = "ColA";
 
-	
-
 	private TestData testData;
-//	private Table table;
+	// private Table table;
 	private TableViewer tableViewer;
 	private WritableList wl;
 
@@ -82,35 +80,32 @@ public class TestWindow extends Shell {
 			}
 		});
 		mntmRefresh.setText("Refresh");
-		
-		
 
 		//
 		//
 		tableViewer.setContentProvider(new ObservableListContentProvider());
 		//
-		 wl = new WritableList(testData.getDataTable(), TestData.Entry.class);
+		wl = new WritableList(testData.getDataTable(), TestData.Entry.class);
 		tableViewer.setInput(wl);
 
-		
 		MenuItem mntmNewRow = new MenuItem(menu, SWT.NONE);
 		mntmNewRow.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				Entry rowAdded = testData.addRow();
-				wl.add(rowAdded); // adding to writeable list updates table and data model
+				wl.add(rowAdded); // adding to writeable list updates table and
+									// data model
 				tableViewer.editElement(rowAdded, 1); // starts editing
 			}
 		});
 		mntmNewRow.setText("New Row");
-		
+
 		MenuItem mntmDebug = new MenuItem(menu, SWT.NONE);
 		mntmDebug.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				ArrayList<Entry> dt = testData.getDataTable();
-				for(Entry et: dt)
-				{
+				for (Entry et : dt) {
 					System.out.println(et.toString());
 				}
 			}
@@ -122,6 +117,7 @@ public class TestWindow extends Shell {
 		// TODO Auto-generated method stub
 		tableViewer = new TableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION);
 		Table table = tableViewer.getTable();
+		TableCursor cursor = new TableCursor(table, SWT.FULL_SELECTION);
 		
 		TextCellEditor te = new TextCellEditor(table);
 
@@ -129,14 +125,13 @@ public class TestWindow extends Shell {
 		table.setHeaderVisible(true);
 		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 
-		
 		TableViewerColumn tvColA = new TableViewerColumn(tableViewer, SWT.NONE);
 		TableColumn tblclmnA = tvColA.getColumn();
 		tblclmnA.setWidth(100);
 		tblclmnA.setText(COL_A);
 		tblclmnA.setAlignment(SWT.RIGHT);
 		tvColA.setLabelProvider(new CellLabelProvider() {
-			
+
 			@Override
 			public void update(ViewerCell cell) {
 				Entry element = (Entry) cell.getElement();
@@ -144,27 +139,29 @@ public class TestWindow extends Shell {
 			}
 		});
 		tvColA.setEditingSupport(new EditingSupport(tableViewer) {
-			
+
 			@Override
 			protected void setValue(Object element, Object value) {
-				Entry e=(Entry) element;
-				String str=(String) value;
+				Entry e = (Entry) element;
+				String str = (String) value;
 				e.setName(str);
-				tableViewer.refresh(e);
+				tableViewer.refresh(e,true);
+				cursor.setSelection(wl.indexOf(e), 0);
+				
 			}
-			
+
 			@Override
 			protected Object getValue(Object element) {
-				Entry e=(Entry) element;
+				Entry e = (Entry) element;
 				return e.getName();
 			}
-			
+
 			@Override
 			protected CellEditor getCellEditor(Object element) {
 				// TODO Auto-generated method stub
 				return te;
 			}
-			
+
 			@Override
 			protected boolean canEdit(Object element) {
 				// TODO Auto-generated method stub
@@ -177,32 +174,30 @@ public class TestWindow extends Shell {
 		tblclmnB.setWidth(100);
 		tblclmnB.setText(COL_B);
 		tblclmnB.setAlignment(SWT.CENTER);
-		 Color bgColor = new Color(parent.getDisplay(),255,128,128);
-		tvColB.setLabelProvider(new StyledCellLabelProvider()  {
-			
+		Color bgColor = new Color(parent.getDisplay(), 255, 128, 128);
+		tvColB.setLabelProvider(new StyledCellLabelProvider() {
+
 			@Override
 			public void update(ViewerCell cell) {
 				Entry element = (Entry) cell.getElement();
-				if(element.getName().isEmpty())
-				{
+				if (element.getName().isEmpty()) {
 					cell.setText("");
+				} else {
+					String num = String.format("%04d", element.getNumber());
+					cell.setText(num);
 				}
-				else
-				{
-				String num = String.format("%04d", element.getNumber());
-				cell.setText(num);
-				}
-				
+
 			}
+
 			@Override
 			protected void paint(Event event, Object element) {
-				int wa=tblclmnB.getWidth();
-				int x=event.x;
-				int y=event.y;
-				int h=event.height-1;
-				Entry e= (Entry) element;
-				int w = (e.getNumber()*wa)/50;
-				GC gc=event.gc;
+				int wa = tblclmnB.getWidth();
+				int x = event.x;
+				int y = event.y;
+				int h = event.height - 1;
+				Entry e = (Entry) element;
+				int w = (e.getNumber() * wa) / 50;
+				GC gc = event.gc;
 				Color oldColor = gc.getBackground();
 				gc.setBackground(bgColor);
 				gc.fillRectangle(x, y, w, h);
@@ -211,30 +206,35 @@ public class TestWindow extends Shell {
 				super.paint(event, element);
 			}
 		});
-		
+
 		tvColB.setEditingSupport(new EditingSupport(tableViewer) {
-			
+
 			@Override
 			protected void setValue(Object element, Object value) {
-					Entry e=(Entry) element;
-					String str=(String) value;
-					e.setNumber(Integer.parseInt(str));
-					tableViewer.refresh(e,true);
+				Entry e = (Entry) element;
+				String str = (String) value;
+				try
+				{
+				e.setNumber(Integer.parseInt(str));
+				}
+				catch(Exception ex)
+				{};
+				tableViewer.refresh(e, true);
 			}
-			
+
 			@Override
 			protected Object getValue(Object element) {
-				
-				Entry e=(Entry) element;
+
+				Entry e = (Entry) element;
 				return String.format("%d", e.getNumber());
 			}
-			
+
 			@Override
 			protected CellEditor getCellEditor(Object element) {
 				// TODO Auto-generated method stub
 				return te;
 			}
-			
+
 			@Override
 			protected boolean canEdit(Object element) {
 				// TODO Auto-generated method stub
@@ -242,85 +242,81 @@ public class TestWindow extends Shell {
 			}
 		});
 		// adds a cursor to the table
-		
+
 	
-	TableCursor cursor = new TableCursor(table, SWT.NONE);
-	cursor.addSelectionListener(new SelectionAdapter() {
-		@Override
-		public void widgetDefaultSelected(SelectionEvent e) {
-			 Entry data2 = (Entry) cursor.getRow().getData();
-			int idx = wl.indexOf(data2);
-			if(idx<0)
-			{
-				
-				System.out.println("adding empty row to data");
-				wl.add(data2);
-				tableViewer.refresh();
+		cursor.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				Entry data2 = (Entry) cursor.getRow().getData();
+				int idx = wl.indexOf(data2);
+				if (idx < 0) {
+
+					System.out.println("adding empty row to data");
+					wl.add(data2);
+					tableViewer.refresh();
+				}
+				tableViewer.editElement(data2, cursor.getColumn());
 			}
-			tableViewer.editElement(data2, cursor.getColumn());
-		}
-	});
-	
-	table.addSelectionListener(new SelectionListener() {
-		
-		@Override
-		public void widgetSelected(SelectionEvent e) {
-			// TODO Auto-generated method stub
-			if(table.getItemCount() <= wl.size())
-			{
-				System.out.println("adding empty row");
-				Entry er = testData.emptyRow();
-				tableViewer.add(er);
-				tableViewer.refresh(er,true);
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				TableItem[] sel = table.getSelection();
+				if ((sel.length != 1) || (sel[0] != cursor.getRow()))
+				{
+					System.out.println("setting table selection");
+					table.setSelection(cursor.getRow());
+				}
 			}
-			if(!cursor.isVisible())
-			{
-				cursor.setSelection(table.getSelectionIndex(),0);
-				cursor.setVisible(true);
+		});
+
+		table.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				if (table.getItemCount() <= wl.size()) {
+					System.out.println("adding empty row");
+					Entry er = testData.emptyRow();
+					tableViewer.add(er);
+					tableViewer.refresh(er, true);
+				}
+				if (cursor.getRow() == null) {
+					System.out.println("cursor not visible");
+					cursor.setSelection(table.getSelectionIndex(), 0);
+					// cursor.setVisible(true);
+					cursor.setFocus();
+				}
 			}
-		}
-		
-		@Override
-		public void widgetDefaultSelected(SelectionEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-	});
-	cursor.addKeyListener(new KeyListener() {
-		
-		@Override
-		public void keyReleased(KeyEvent e) {
-			if(e.keyCode==SWT.INSERT)
-			{
-				Entry er = testData.emptyRow();
-				wl.add(table.getSelectionIndex(), er);
-				tableViewer.refresh(er,true);
-				tableViewer.editElement(er, 0);
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
+
 			}
-			if(e.keyCode==SWT.DEL)
-			{
-				wl.remove(table.getSelectionIndex());
+		});
+		cursor.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.keyCode == SWT.INSERT) {
+					Entry er = testData.emptyRow();
+					wl.add(table.getSelectionIndex(), er);
+					tableViewer.refresh(er, true);
+					tableViewer.editElement(er, 0);
+				}
+				if (e.keyCode == SWT.DEL) {
+					wl.remove(table.getSelectionIndex());
+				}
 			}
-		}
-		
-		@Override
-		public void keyPressed(KeyEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-	});
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 		//
-//	cursor.addListener(SWT.FocusIn,  new  Listener() {
-//		
-//		@Override
-//		public void handleEvent(Event event) {
-//			if(table.getItemCount()<= wl.size())
-//			{
-//				System.out.println("adding empty row on focus in");
-//				tableViewer.add(testData.emptyRow());
-//			}
-//		}
-//	});
+
 		//
 	}
 
