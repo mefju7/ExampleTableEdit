2ce67576aa73d3a8071709e126874b619422fb5e
Matthias P. Nowak <matthias.nowak@marintek.sintef.no>
2015-04-16 14:16:05 +0200

    simpler with editorsupport

--- src/marintek/tableEditor/TestWindow.java
+++ src/marintek/tableEditor/TestWindow.java
@@ -7,16 +7,15 @@ import marintek.tableEditor.TestData.Entry;
 import org.eclipse.core.databinding.observable.list.WritableList;
 import org.eclipse.jface.databinding.viewers.ObservableListContentProvider;
 import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.ICellModifier;
-import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -26,13 +25,11 @@ import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 
 public class TestWindow extends Shell {
 
 	private static final String COL_B = "ColB";
 	private static final String COL_A = "ColA";
-	private static String[] TableColumnNames = new String[] { COL_A, COL_B };
 
 	
 
@@ -80,9 +77,6 @@ public class TestWindow extends Shell {
 		//
 		//
 		tableViewer.setContentProvider(new ObservableListContentProvider());
-		addLabelProvider(tableViewer);
-		addCellModifier(tableViewer);
-		
 		//
 		WritableList wl = new WritableList(testData.getDataTable(), TestData.Entry.class);
 		tableViewer.setInput(wl);
@@ -112,118 +106,101 @@ public class TestWindow extends Shell {
 		mntmDebug.setText("Debug");
 	}
 
-	private void addCellModifier(TableViewer tabView) {
-		tabView.setColumnProperties(TableColumnNames); // needed for modifier
-		tabView.setCellModifier(new ICellModifier()
-		{
-
-			@Override
-			public boolean canModify(Object element, String property) {
-				// TODO Auto-generated method stub
-				Entry e = (Entry) element;
-				System.out.printf("canModify %s(%s)\n", e.toString(), property);
-				return true;
-			}
-
-			@Override
-			public Object getValue(Object element, String property) {
-				Entry e = (Entry) element;
-				System.out.printf("getValue %s(%s)\n", e.toString(), property);
-				if (COL_A.equals(property))
-					return e.getName();
-				if (COL_B.equals(property))
-					return String.format("%d", e.getNumber());
-				return null;
-			}
-
-			@Override
-			public void modify(Object element, String property, Object value) {
-				// using the Entry from getValue call
-				TableItem ti = (TableItem) element;
-				 Entry e = (Entry) ti.getData();
-				String str = (String) value;
-				System.out.printf("modify %s(%s)=%s\n", e.toString(), property, str);
-				if (COL_A.equals(property))
-				{
-					e.setName(str);
-				}
-				if (COL_B.equals(property))
-				{
-					e.setNumber(Integer.parseInt(str));
-				}			
-				tabView.refresh(e,true);
-			}
-
-		});
-		// now adding editor part
-		Table table = tableViewer.getTable();
-		CellEditor[] editors = new CellEditor[2];
-		editors[0] = new TextCellEditor(table);
-		editors[1] = new TextCellEditor(table);
-		tabView.setCellEditors(editors);
-	}
-
-	private void addLabelProvider(TableViewer tabView) {
-		// table = tableViewer.getTable();
-		tableViewer.setLabelProvider(new ITableLabelProvider() {
-			@Override
-			public Image getColumnImage(Object element, int columnIndex) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public String getColumnText(Object element, int columnIndex) {
-				Entry e = (Entry) element;
-				switch (columnIndex) {
-				case 0:
-					return e.getName();
-				case 1:
-					return String.format("%03d", e.getNumber());
-
-				}
-				return null;
-			}
-
-			@Override
-			public void addListener(ILabelProviderListener listener) {
-			}
-
-			@Override
-			public void dispose() {
-			}
-
-			@Override
-			public boolean isLabelProperty(Object element, String property) {
-				return false;
-			}
-
-			@Override
-			public void removeListener(ILabelProviderListener listener) {
-			}
-		});
-
-	}
-
 	private void createContent(Composite parent) {
 		// TODO Auto-generated method stub
 		tableViewer = new TableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION);
 		Table table = tableViewer.getTable();
+		TextCellEditor te = new TextCellEditor(table);
+
 		table.setLinesVisible(true);
 		table.setHeaderVisible(true);
 		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 
-		TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
-		TableColumn tblclmnA = tableViewerColumn.getColumn();
+		TableViewerColumn tvColA = new TableViewerColumn(tableViewer, SWT.NONE);
+		TableColumn tblclmnA = tvColA.getColumn();
 		tblclmnA.setWidth(100);
 		tblclmnA.setText(COL_A);
-		
+		tvColA.setLabelProvider(new CellLabelProvider() {
+			
+			@Override
+			public void update(ViewerCell cell) {
+				Entry element = (Entry) cell.getElement();
+				cell.setText(element.getName());
+			}
+		});
+		tvColA.setEditingSupport(new EditingSupport(tableViewer) {
+			
+			@Override
+			protected void setValue(Object element, Object value) {
+				Entry e=(Entry) element;
+				String str=(String) value;
+				e.setName(str);
+				tableViewer.refresh(e);
+			}
+			
+			@Override
+			protected Object getValue(Object element) {
+				Entry e=(Entry) element;
+				return e.getName();
+			}
+			
+			@Override
+			protected CellEditor getCellEditor(Object element) {
+				// TODO Auto-generated method stub
+				return te;
+			}
+			
+			@Override
+			protected boolean canEdit(Object element) {
+				// TODO Auto-generated method stub
+				return true;
+			}
+		});
 
-		TableViewerColumn tableViewerColumn_1 = new TableViewerColumn(tableViewer, SWT.NONE);
-		TableColumn tblclmnB = tableViewerColumn_1.getColumn();
+		TableViewerColumn tvColB = new TableViewerColumn(tableViewer, SWT.NONE);
+		TableColumn tblclmnB = tvColB.getColumn();
 		tblclmnB.setWidth(100);
 		tblclmnB.setText(COL_B);
 		tblclmnB.setAlignment(SWT.CENTER);
+		tvColB.setLabelProvider(new CellLabelProvider() {
+			
+			@Override
+			public void update(ViewerCell cell) {
+				Entry element = (Entry) cell.getElement();
+				cell.setText(String.format("%04d", element.getNumber()));
+				
+			}
+		});
+		
+		tvColB.setEditingSupport(new EditingSupport(tableViewer) {
+			
+			@Override
+			protected void setValue(Object element, Object value) {
+					Entry e=(Entry) element;
+					String str=(String) value;
+					e.setNumber(Integer.parseInt(str));
+					tableViewer.refresh(e,true);
+			}
+			
+			@Override
+			protected Object getValue(Object element) {
+				
+				Entry e=(Entry) element;
+				return String.format("%d", e.getNumber());
+			}
+			
+			@Override
+			protected CellEditor getCellEditor(Object element) {
+				// TODO Auto-generated method stub
+				return te;
+			}
+			
+			@Override
+			protected boolean canEdit(Object element) {
+				// TODO Auto-generated method stub
+				return true;
+			}
+		});
 		//
 		//
 	}
